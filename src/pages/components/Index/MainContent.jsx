40b94bf4148cfd93a22c36b24c02732f5d10844e
thedanielmark <danielmark.uc@gmail.com>
2020-11-14 19:12:15 +0530

    .Initial commit

--- src/pages/components/Index/MainContent.jsx
+++ src/pages/components/Index/MainContent.jsx
@@ -61,9 +61,32 @@ export default function MainContent() {
 
   return (
     <motion.div exit={{ opacity: 0 }} initial="initial" animate="animate">
-      <motion.div
-        variants={fade1}
-      >
+      <motion.div variants={fade1}>
+        <div
+          className="d-flex align-items-center index-section-1"
+          style={{ padding: "300px 0px" }}
+        >
+          <div className="container d-flex justify-content-center justify-content-around-lg">
+            <div className="col-7">
+              <h1 className="text-white text-uppercase font-weight-bold">
+                Taste the victory
+              </h1>
+              <h4 className="text-white display-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Dolorem, ipsum reprehenderit?
+              </h4>
+              <div className="mt-5 d-flex jutify-content-center justify-content-start">
+                <button type="button" className="btn btn-secondary btn-lg mr-lg-3">
+                  Primary
+                </button>
+                <button type="button" className="btn btn-outline-primary btn-lg">
+                  Primary
+                </button>
+              </div>
+            </div>
+            <div className="col-5"></div>
+          </div>
+        </div>
       </motion.div>
     </motion.div>
   );
